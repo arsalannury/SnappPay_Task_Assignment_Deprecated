df29cd2d72d2ce83fd355bb1f964c5d5b4e1f61a
ArsalanNury <arsiiiland@yahoo.com>
2024-02-29 19:54:43 +0330

    get all contacts and set props to contactCart

--- src/components/contactcart/ContactCart.jsx
+++ src/components/contactcart/ContactCart.jsx
@@ -1,36 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ContactCart = (props) => {
+const ContactCart = ({ avatar, name, phone, city, id }) => {
   return (
     <>
-      <div className="flow-root">
-        <ul
-          role="list"
-          className="divide-y divide-gray-200 dark:divide-gray-700"
-        >
-          <li className="py-3 sm:py-4">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <img
-                  className="w-8 h-8 rounded-full"
-                  src="https://robohash.org/nihildictadistinctio.png?size=300x300&set=set1"
-                  alt="Neil image"
-                />
+      <div className="flow-root border-b-2">
+        <Link to={`/detailPage/${id}`}>
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-gray-700"
+          >
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src={avatar}
+                    alt="avatar image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    {name}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    address: {city ?? "----"}
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-blue-950 dark:text-white">
+                  +{phone}
+                </div>
               </div>
-              <div className="flex-1 min-w-0 ms-4">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                  Neil Sims
-                </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                  email@windster.com
-                </p>
-              </div>
-              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                $320
-              </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </Link>
       </div>
     </>
   );
--- src/layouts/home/HomePage.jsx
+++ src/layouts/home/HomePage.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBox from "../../components/searchbox/SearchBox";
 import VisitedContacts from "../../components/visitedcontacts/VisitedContacts";
 import ContactCart from "../../components/contactcart/ContactCart";
+import { BaseUrl } from "../../baseUrl";
+import Loading from "../../components/loading/Loading";
 
 const HomePage = () => {
+  const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getAllContacts = async () => {
+    setLoading(true);
+    try {
+      const fetchResult = await BaseUrl.get("/passenger");
+      setContacts(fetchResult.data.items);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const renderContent = () => {
+    if (loading) return <Loading />;
+      
+    if (contacts && contacts.length > 0) {
+      return contacts.map((contact) => (
+        <ContactCart
+          key={contact.id}
+          id={contact.id}
+          avatar={contact.avatar}
+          city={contact.address}
+          name={`${contact.first_name} ${contact.last_name}`}
+          phone={contact.phone}
+        />
+      ));
+    }
+  };
+
+  useEffect(() => {
+    getAllContacts();
+    return () => setContacts([]);
+  }, []);
+
   return (
     <>
       <div className="md:w-8/12 w-full m-auto  p-3">
@@ -13,9 +51,7 @@ const HomePage = () => {
           <VisitedContacts />
         </div>
         <p className="my-10 sm:text-left text-center">Contacts</p>
-        <div>
-          <ContactCart />
-        </div>
+        <div>{renderContent()}</div>
       </div>
     </>
   );
