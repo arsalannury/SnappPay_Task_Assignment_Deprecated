b8a97996bb785098b992b26fd03cec6362a01ceb
ArsalanNury <arsiiiland@yahoo.com>
2024-02-29 18:26:29 +0330

    add contact cart simple UI

--- src/components/searchbox/SearchBox.jsx
+++ src/components/searchbox/SearchBox.jsx
@@ -5,7 +5,7 @@ const SearchBox = () => {
     <div className="flex items-center justify-center flex-col border-b-2 pb-10">
       <div className="md:w-4/12 w-full">
         <label
-          for="contactnumber"
+          htmlFor="contactnumber"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4"
         >
           Contact Number
@@ -13,12 +13,12 @@ const SearchBox = () => {
         <input
           type="text"
           id="contactnumber"
-          class="bg-gray-50 border text-left w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
+          className="bg-gray-50 border text-left w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
         />
       </div>
       <div className="md:w-4/12 w-full">
         <label
-          for="contactname"
+          htmlFor="contactname"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4"
         >
           Contact Name
@@ -26,7 +26,7 @@ const SearchBox = () => {
         <input
           type="text"
           id="contactname"
-          class="bg-gray-50 border text-left w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
+          className="bg-gray-50 border text-left w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
         />
       </div>
       <button className="px-12 py-2 mt-5 bg-blue-950 text-white rounded-xl">
--- src/layouts/home/HomePage.jsx
+++ src/layouts/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBox from "../../components/searchbox/SearchBox";
 import VisitedContacts from "../../components/visitedcontacts/VisitedContacts";
+import ContactCart from "../../components/contactcart/ContactCart";
 
 const HomePage = () => {
   return (
@@ -8,8 +9,12 @@ const HomePage = () => {
       <div className="md:w-8/12 w-full m-auto  p-3">
         <SearchBox />
         <p className="my-10 sm:text-left text-center">Last Visited Contacts</p>
-        <div className="flex justify-around items-center sm:flex-row flex-col">
+        <div className="flex justify-around items-center sm:flex-row flex-col pb-10 border-b-2">
           <VisitedContacts />
+        </div>
+        <p className="my-10 sm:text-left text-center">Contacts</p>
+        <div>
+          <ContactCart />
         </div>
       </div>
     </>
