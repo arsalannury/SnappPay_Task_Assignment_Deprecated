f21562371ca2a15f9dbc2bd5911dbe04d7554701
ArsalanNury <arsiiiland@yahoo.com>
2024-02-29 17:53:46 +0330

    add hover effect to last visit cart

--- src/components/visitedcontacts/VisitedContacts.jsx
+++ src/components/visitedcontacts/VisitedContacts.jsx
@@ -4,8 +4,8 @@ import Contact from "../../assets/images/contact.png";
 
 const VisitedContacts = (props) => {
   return (
-    <div className="flex items-center p-10 m-5 border border-blue-950 rounded-md bg-white shadow-md">
-      <Link to={"/"} className="flex items-center justify-center flex-col">
+    <div className="flex items-center  m-5 border border-blue-950 rounded-md bg-white shadow-md transition-all hover:shadow-lg hover:-translate-y-3">
+      <Link to={"/"} className="flex items-center justify-center flex-col p-10">
         <img src={Contact} alt="contact icon" />
         <p>Arsalan Nury</p>
       </Link>
