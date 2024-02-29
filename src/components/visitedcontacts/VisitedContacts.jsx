38d69ee2c81d5d62863ba87a6dc6051680ff1591
ArsalanNury <arsiiiland@yahoo.com>
2024-02-29 20:46:31 +0330

    stop last visited for now

--- src/components/visitedcontacts/VisitedContacts.jsx
+++ src/components/visitedcontacts/VisitedContacts.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Contact from "../../assets/images/contact.png";
 
-const VisitedContacts = (props) => {
+const VisitedContacts = ({ name, path }) => {
   return (
     <div className="flex items-center  m-5 border border-blue-950 rounded-md bg-white shadow-md transition-all hover:shadow-lg hover:-translate-y-3">
-      <Link to={"/"} className="flex items-center justify-center flex-col p-10">
+      <Link
+        to={`/detailPage/${path}`}
+        className="flex items-center justify-center flex-col p-10"
+      >
         <img src={Contact} alt="contact icon" />
-        <p>Arsalan Nury</p>
+        <p>{name}</p>
       </Link>
     </div>
   );
--- src/layouts/home/HomePage.jsx
+++ src/layouts/home/HomePage.jsx
@@ -4,6 +4,7 @@ import VisitedContacts from "../../components/visitedcontacts/VisitedContacts";
 import ContactCart from "../../components/contactcart/ContactCart";
 import { BaseUrl } from "../../baseUrl";
 import Loading from "../../components/loading/Loading";
+import Oops from "../../assets/images/oops.png";
 
 const HomePage = () => {
   const [contacts, setContacts] = useState([]);
@@ -20,9 +21,9 @@ const HomePage = () => {
     }
   };
 
-  const renderContent = () => {
+  const renderContacts = () => {
     if (loading) return <Loading />;
-      
+
     if (contacts && contacts.length > 0) {
       return contacts.map((contact) => (
         <ContactCart
@@ -37,6 +38,30 @@ const HomePage = () => {
     }
   };
 
+  const renderLastVisited = () => {
+    const lastVisitedList =
+      localStorage.getItem("last_visited") &&
+      JSON.parse(localStorage.getItem("last_visited"));
+
+    if (lastVisitedList) {
+      return lastVisitedList.map((visit) => (
+        <VisitedContacts name={visit.name} path={visit.id} />
+      ));
+    }
+
+    return (
+      <div>
+        <img
+          src={Oops}
+          lazy={true}
+          alt="oops image"
+          className="w-[150px] m-auto"
+        />
+        <p>Oops! The Last Visited List Is Empty</p>
+      </div>
+    );
+  };
+
   useEffect(() => {
     getAllContacts();
     return () => setContacts([]);
@@ -48,10 +73,10 @@ const HomePage = () => {
         <SearchBox />
         <p className="my-10 sm:text-left text-center">Last Visited Contacts</p>
         <div className="flex justify-around items-center sm:flex-row flex-col pb-10 border-b-2">
-          <VisitedContacts />
+          {renderLastVisited()}
         </div>
         <p className="my-10 sm:text-left text-center">Contacts</p>
-        <div>{renderContent()}</div>
+        <div>{renderContacts()}</div>
       </div>
     </>
   );
